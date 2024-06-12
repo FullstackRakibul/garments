c2b3a9aa6b7727ea7972343090c6649aa069d3aa
unknown <rakibul.cse.47@gmail.com>
2024-06-12 23:13:25 +0600

    contact form add in contact page

--- src/component/ui/ContactMap/index.jsx
+++ src/component/ui/ContactMap/index.jsx
@@ -1,78 +1,106 @@
-import {React } from 'react'
-import {Col, Row,Form} from "antd";
+import React from "react";
+import { Row, Col, Form, Input, Checkbox, Button } from "antd";
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  InstagramOutlined,
+  LinkedinOutlined,
+  AlignLeftOutlined,
+} from "@ant-design/icons";
 
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
 
-const mapStyle = {
-    width:"100%" ,height:"450px" ,frameBorder:"0" ,scrolling:"no" ,marginHeight:"0" ,marginWidth:"0"
-}
-
-
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
 
 const ContactMap = () => {
-    return (<>
-            <section>
-                <Row>
-                    <Col span={10}>
-                        <div>
-                            <p>Contact Us</p>
-                            <h3>Get In Touch</h3>
-                        </div>
-                        <div>
-                {/*            <Form*/}
-                {/*                name="basic"*/}
-                {/*                labelCol={{ span: 8 }}*/}
-                {/*                wrapperCol={{ span: 16 }}*/}
-                {/*                style={{ maxWidth: 600 }}*/}
-                {/*                initialValues={{ remember: true }}*/}
-                {/*                onFinish={onFinish}*/}
-                {/*                onFinishFailed={onFinishFailed}*/}
-                {/*                autoComplete="off"*/}
-                {/*            >*/}
-                {/*                <Form.Item<FieldType>*/}
-                {/*                    label="Username"*/}
-                {/*                    name="username"*/}
-                {/*                    rules={[{ required: true, message: 'Please input your username!' }]}*/}
-                {/*                    >*/}
-                {/*                    <Input />*/}
-                {/*            </Form.Item>*/}
+  return (
+    <>
+      <section className="container mx-auto p-20 ">
+        <Row gutter={16} className="flex justify-between items-center">
+          <Col span={10} className="flex flex-col p-24 justify-center">
+            <div className="mb-4">
+              <p className="text-xl font-semibold">Contact Us</p>
+              <h3 className="text-5xl font-bold">Get In Touch</h3>
+            </div>
+            <div className="flex space-x-4">
+              <FacebookOutlined className="text-3xl text-blue-600" />
+              <TwitterOutlined className="text-3xl text-blue-400" />
+              <InstagramOutlined className="text-3xl text-pink-600" />
+              <LinkedinOutlined className="text-3xl text-blue-700" />
+            </div>
+          </Col>
+          <Col span={14}>
+            <div>
+              <Form
+                name="basic"
+                // labelCol={{ span: 8 }}
+                // wrapperCol={{ span: 16 }}
+                // style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                className="p-6 "
+              >
+                <Form.Item
+                  name="mail"
+                  rules={[
+                    {
+                      initialValues: true,
+                      required: true,
+                      message: "Enter your mail",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
 
-                {/*            <Form.Item<FieldType>*/}
-                {/*                label="Password"*/}
-                {/*                name="password"*/}
-                {/*                rules={[{ required: true, message: 'Please input your password!' }]}*/}
-                {/*                >*/}
-                {/*                <Input.Password />*/}
-                {/*        </Form.Item>*/}
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      initialValues: true,
+                      required: true,
+                      message: "Write message",
+                    },
+                  ]}
+                >
+                  <Input.TextArea rows={4} />
+                </Form.Item>
 
-                {/*        <Form.Item<FieldType>*/}
-                {/*            name="remember"*/}
-                {/*            valuePropName="checked"*/}
-                {/*            wrapperCol={{ offset: 8, span: 16 }}*/}
-                {/*            >*/}
-                {/*            <Checkbox>Remember me</Checkbox>*/}
-                {/*    </Form.Item>*/}
-
-                {/*    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>*/}
-                {/*        <Button type="primary" htmlType="submit">*/}
-                {/*            Submit*/}
-                {/*        </Button>*/}
-                {/*    </Form.Item>*/}
-                {/*</Form>*/}
-                        </div>
-                    </Col>
-                    <Col span={10}>
-
-                    </Col>
-                </Row>
-            </section>
-        <section>
-                <iframe style={mapStyle}
-                        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=24.095866638019977,%2090.16833957812368+(Explore%20Garments%20Ltd.)&amp;t=h&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                    <a href="https://www.gps.ie/">gps vehicle tracker</a>
-                </iframe>
-        </section>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit">
+                    Send Message
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </section>
+      <section className="container mx-auto p-6">
+        <Row>
+          <div>
+            <h3 className="text-xl">Find us on Map</h3>
+          </div>
+          <Col span={24}>
+            <iframe
+              src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=24.095866638019977,%2090.16833957812368+(Explore%20Garments%20Ltd.)&amp;t=h&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              className="w-full h-96"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Google Maps"
+            ></iframe>
+          </Col>
+        </Row>
+      </section>
     </>
-)
-}
+  );
+};
 
 export default ContactMap;
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,50 +1,46 @@
-import React, { Fragment } from 'react'
-import { Breadcrumb, Layout, Menu,FloatButton} from 'antd';
-import TopNavBar from '../../component/TopNavBar';
-import PageHeroBg from '../../component/HeroBg';
-import ContactInfo from '../../component/ContactInfo';
-import FooterComponent from '../../component/Footer';
-import ContactMap from '../../component/ui/ContactMap/index';
+import React, { Fragment } from "react";
+import { Breadcrumb, Layout, Menu, FloatButton } from "antd";
+import TopNavBar from "../../component/TopNavBar";
+import PageHeroBg from "../../component/HeroBg";
+import ContactInfo from "../../component/ContactInfo";
+import FooterComponent from "../../component/Footer";
+import ContactMap from "../../component/ui/ContactMap/index.jsx";
 const { Header, Content, Footer } = Layout;
 
-class ContactPages extends React.Component{
-    render(){
-        return(
-            <Fragment>
-                <Layout>
-                    <Header
-                    style={{
-                        position:"fixed",
-                        top: 0,
-                        overflow:"auto",
-                        zIndex:1500,
-                        width: '100%',
-                        borderBottom:"1px solid grey"
-                      }}
-                    >
-                        <TopNavBar title="Contact"/>
-                    </Header>
-                    <Content>
-                        <PageHeroBg bgTitle="Contact"/>
-                        <ContactMap/>
-                        <ContactInfo/>
-                    </Content>
-                    <Footer
-                    style={{
-                        textAlign:"center"
-                    }}
-                    >
-                        <FloatButton.BackTop
-                        duration="1000"
-                        />
-                        <FooterComponent/>
-                        <span>
-                            Copyright © Ha-Meem Group All Right Reserved.
-                        </span>
-                    </Footer>
-                </Layout>
-            </Fragment>
-        )
-    }
+class ContactPages extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <Layout>
+          <Header
+            style={{
+              position: "fixed",
+              top: 0,
+              overflow: "auto",
+              zIndex: 1500,
+              width: "100%",
+              borderBottom: "1px solid grey",
+            }}
+          >
+            <TopNavBar title="Contact" />
+          </Header>
+          <Content>
+            <PageHeroBg bgTitle="Contact" />
+            <ContactMap />
+            <ContactInfo />
+          </Content>
+          <Footer
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <FloatButton.BackTop duration="1000" />
+            <FooterComponent />
+            <span>Copyright © Ha-Meem Group All Right Reserved.</span>
+          </Footer>
+        </Layout>
+      </Fragment>
+    );
+  }
 }
 export default ContactPages;
